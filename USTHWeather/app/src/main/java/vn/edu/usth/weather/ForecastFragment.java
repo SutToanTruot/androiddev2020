5217fabb9b74e94de6b59418482ee992b15af5a9
SutToanTruot <hiepnguyenhd8799@gmail.com>
2019-11-20 18:08:35 +0700

    update labwork 4

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -1,11 +1,11 @@
 package vn.edu.usth.weather;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,9 +17,7 @@ public class ForecastFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = new View(getContext());
-        v.setBackgroundColor(0x66FFCC);
-        return v;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
+        return inflater.inflate(R.layout.fragment_forecast,container,false);
     }
 }
