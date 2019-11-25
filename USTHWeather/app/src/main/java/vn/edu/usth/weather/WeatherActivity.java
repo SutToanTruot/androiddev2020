b6591d6024b594d51cfb314b679c4c289c832179
SutToanTruot <hiepnguyenhd8799@gmail.com>
2019-11-25 19:32:02 +0700

    labwork 8

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,32 +1,23 @@
 package vn.edu.usth.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.AttributeSet;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
-
+    private ViewPager viewPager;
+    private Adapter adapter;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        ForecastFragment firstFragment = new ForecastFragment();
-        WeatherFragment secondFragment = new WeatherFragment();
         Log.i("create", "Create done");
-        ForecastFragment firstFragment = new ForecastFragment ();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+        viewPager = findViewById(R.id.WeatherForecastViewPager);
+        adapter = new Adapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
 
     }
     @Override
--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherAndForcastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherAndForcastFragment.java
@@ -1,13 +1,25 @@
 package vn.edu.usth.weather;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-public class WeatherAndForcastFragment extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather_and_forcast_fragment);
+public class WeatherAndForecastFragment extends Fragment {
+    public WeatherAndForecastFragment(){
     }
+
+    @Nullable
+    @Override
+    public class WeatherAndForecastFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.activity_weather_and_forcast_fragment, container, false);
+        }
 }
